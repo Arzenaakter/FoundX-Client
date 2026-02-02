53dfbf3d4180ce7a2a1ab4f0c8d9ea9b001d2d22
Arzenaakter <arzenaakter@gmail.com>
2026-02-02 17:03:36 +0600

    Setting up the form for the create post page

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import FXInput from "@/src/components/form/FXInput";
+import { Button } from "@heroui/button";
 import React from "react";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 
-const page = () => {
-  return <div>create post</div>;
+const CreatePost = () => {
+  const methods = useForm();
+  const { control, handleSubmit } = methods;
+  const { append, remove } = useFieldArray({
+    control,
+    name: "questions",
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FXInput name="title" label="Title" />
+        <Button type="submit">Create Post</Button>
+      </form>
+    </FormProvider>
+  );
 };
 
-export default page;
+export default CreatePost;
